af91eef3208e793d451a2910797d48437b2f93b5
Valentina Garzon <valentinagp2006@gmail.com>
2026-01-27 19:16:30 -0500

    Fix typos in Taller 1.cs methods

--- Taller 1.cs
+++ Taller 1.cs
@@ -13,8 +13,8 @@ double CalcularAreaCirculo(double radio) { return 0; }
 int ObtenerMaximo(List numeros) { return 0; }
 int ObtenerMinimo(List numeros) { return 0; }
 void ImprimirLista(List elementos) { }
-List ConvertirAMayusculas(List textos) { retunr new List(); }
+List ConvertirAMayusculas(List textos) { return new List(); }
 int ContarElementos(List numeros) { return 0; }
 bool ContieneElemento(List numeros, int valor) {return false; }
-double CalcularPromediosNotas(double[] notas) { return 0, }
+double CalcularPromediosNotas(double[] notas) { return 0; }
 string ObtenerIniciales(string nombreCompleto) { return ""; }
